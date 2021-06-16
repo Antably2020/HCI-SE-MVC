94b051c475c4ba054a085a582b72915764b41a48
Kareem Elantably <73814564+Antably2020@users.noreply.github.com>
2021-06-16 23:00:20 +0200

    Update A_productsModel.php

--- app/models/A_productsModel.php
+++ app/models/A_productsModel.php
@@ -1,5 +1,12 @@
 <?php
 class A_productsModel extends model{
 public $title="Products";
+
+
+public function readProduct()
+{
+    $this->dbh->query("SELECT * FROM products");
+    return $this->dbh->resultSet();
+}
 }
 ?>
