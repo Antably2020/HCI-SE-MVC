5d30445aa9f50c8e31ec43ca5d62b77511f05aed
ATamer24 <mido.tamer2000@hotmail.com>
2021-06-17 03:56:50 +0200

    contact us updated

--- app/views/pages/contact.php
+++ app/views/pages/contact.php
@@ -6,43 +6,40 @@ class contact extends view{
     require APPROOT . '/views/inc/header.php';
  ?>
 
-<div class="login-container " style="border-bottom:0px; border-top: 5px solid #FF7A00; ">
-
-
-
-  <form class="form" method="post" action="" name="Submit" >
-       
-    <h1 style=" padding-top: 105px;">Contact Us<h1>
-      
-		<div  >
-	
-						
-						<input class="login-field" type="text"  required="true"  name="email" placeholder="Email Address" autofocus="false">
-						
-		
-			
-			
-	
-            <select class="form-select login-field" name="choice" aria-label="Default select example">
-  <option selected>What is this about?</option>
-  <option value="1">complain</option>
+      <!--      <select class="form-select login-field" name="choice" aria-label="Default select example">
+  <option value="none" selected disabled hidden>Select</option>
+  <option value="1">Orders and shipping</option>
   <option value="2">option 2</option>
   <option value="3">option 3</option>
 </select>
 <textarea class="form-control login-field" id="exampleFormControlTextarea1" rows="3" name="desc" placeholder="Description"style="  display: block; margin-left:auto; margin-right: auto;"></textarea>
-					</div>
-				
-					
-					
-					
-        <input type="submit" class="login-btn" id="Login" name="submit"onsubmit="return false" value="submit" class="login-button">
-        <input type="button" class="login-btn" onclick="window.location.href='<?php echo URLROOT . 'public'; ?>';" value="Back" />
-      
-  
-	  </form>  
-  
-     </div>
+-->
 
+
+<div class="login-container ">
+    
+    <form class="form" method="post" action="post" name="Contact" style="padding: 18px;" >
+         <div style="margin-top:10px;">
+<select class="btn  dropdown-toggle" name="choice" style="width:60%; border:2px solid #FF7A00;">
+  <option value="none" selected disabled hidden>Select</option>
+  <option value="1">Exchanges</option>
+  <option value="2">Warranty</option>
+  <option value="3">Shipping</option>
+  <option value="4">Gifts</option>
+</select>
+         </div>
+      <div  style=" padding-top: 30px; ">
+              <div ><label >Descrption</label></div>
+              <textarea class="form-control center-block" style="width:60% ;border:2px solid #FF7A00;" type="text" required="true" name="" ></textarea>
+            </div>
+          
+            
+            
+            
+          <input type="submit" class="login-btn" id="Login" name="Login"onsubmit="return false" value="Submit" class="login-button">
+          
+        
+      </form>     </div>
 
  <?php
   require APPROOT . '/views/inc/footer.php';
